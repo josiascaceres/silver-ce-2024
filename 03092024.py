1d7511b2a460a2c633acfdfdd8216148429c4580
Samuel Cáceres <144251952+samuelcaceres@users.noreply.github.com>
2024-03-25 22:12:25 -0400

    Subir carpeta, primer commit

---
+++ 03092024.py
@@ -0,0 +1,76 @@
+#5 
+"""
+numero_1 = int(input('Ingrese el primer numero: '))
+numero_2 = int(input('Ingrese el segundo numero: '))
+
+resultado = numero_1 // numero_2
+
+print('El resultado es: ' + str(resultado))
+"""
+
+
+#6
+"""base = int(input('Ingrese la base: '))
+altura = int(input('Ingrese la altura: '))
+
+area = (base * altura) / 2
+
+print('El area es ' + str(area))"""
+
+
+#9
+"""numero = int(input('Ingrese el numero: '))
+
+numero_str = str(numero) + " "
+
+print(numero_str * numero)"""
+
+
+#10
+"""celsius = float(input('Ingrese grados en celsius: '))
+
+fahrenheit = (celsius * (9 / 5)) + 32
+
+print(str(fahrenheit) + ' grados fahrenheit, es equivalente a ' + str(celsius) + ' grados celsius.')"""
+
+
+
+#LOWER
+"""palabra = 'JOSIAS'
+minuscula = palabra.lower()
+
+print(minuscula)"""
+
+#UPPER
+
+"""palabra = 'josias'
+mayuscula = palabra.upper()
+
+print(mayuscula)"""
+
+#Title
+"""palabra = 'josias samuel caceres'
+titulo = palabra.title()
+
+print(titulo)"""
+
+
+#Replace 
+"""palabra = 'Paraguay'
+
+palabra_x = palabra.replace('a','')
+
+print(palabra_x)"""
+
+
+#Ejemplo replace
+"""frase = 'Hola mundo'
+
+sin_espacio = frase.replace(' ', '')
+
+longitud = len(sin_espacio)
+
+print(longitud)"""
+
+
+HDAFGADGAG
---
+++ UNI/1_manipulacion_strings.py
@@ -0,0 +1,76 @@
+#1
+"""
+nombre_usuario = "Josias"
+numero_entero = 2
+
+print(nombre_usuario * numero_entero)"""
+
+#2 
+"""
+nombre_usuario = "Josias"
+apellido_usuario = "Caceres"
+
+nombre_completo = nombre_usuario + " " + apellido_usuario
+
+numero_entero = 10
+
+print((nombre_completo + '\n') * numero_entero)"""
+
+#3
+"""
+nombre = 'Josias'
+apellido = 'Caceres'
+
+nombre_completo = nombre + ' ' + apellido
+
+n = len(nombre_completo.replace(' ', ''))
+
+print(nombre_completo + ' tiene ' + str(n) + ' letras.')
+"""
+
+#4
+"""
+producto = input('Ingrese el nombre del producto: ')
+precio_unitario = int(input('Ingrese el precio unitario del producto: '))
+cantidad = int(input('Ingrese la cantidad de unidades: '))
+
+precio_total = precio_unitario * cantidad
+
+cantidad_str = str(cantidad)
+precio_unitario_str = str(precio_unitario)
+precio_total_str = str(precio_total)
+
+longitud_precio_unitario_str = len(precio_unitario_str)
+longitud_cantidad_str = len(cantidad_str)
+longitud_precio_total_str = len(precio_total_str)
+
+precio_unitario_final = ('0' * (6 - longitud_precio_unitario_str)) + precio_unitario_str
+cantidad_final = ('0' * (3 - longitud_cantidad_str)) + cantidad_str
+precio_total_final = ('0' * (8 - longitud_precio_total_str)) + precio_total_str
+
+guion = '_'
+
+print(producto + guion + precio_unitario_final + guion + cantidad_final + guion + precio_total_final)
+"""
+
+#5
+"""
+frase = 'hola a todos'
+vocal = 'a' 
+frase += vocal.upper()
+
+print(frase)"""
+
+#6
+"""
+texto = 'Hola Mundo'
+n = 5
+
+print((texto + ' ') * n)"""
+
+#7
+"""nombre_libro = 'Harry Potter'
+
+cantidad_letras = len(nombre_libro.replace(' ', ''))
+
+print('El libro ' + nombre_libro + ' tiene ' + str(cantidad_letras) + 'caracteres.')"""
