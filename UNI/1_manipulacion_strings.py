1f6b06bdcebf1a926dded0541a5d95aeed80746f
Samuel Cáceres <144251952+samuelcaceres@users.noreply.github.com>
2024-03-26 11:05:48 -0400

    Actualizacion
    Actualizacion hecha en la clase virtual de semana santa

--- UNI/1_manipulacion_strings.py
+++ UNI/1_manipulacion_strings.py
@@ -2,80 +2,104 @@
 Este es el solucionario del ejercitario de manipulacion de Strings de la UNI. Ante cualquier consulta:
 """
 
-
 #1
 """
-nombre_usuario = "Josias"
-numero_entero = 2
+nombre = 'Juan'
+entero = 4
 
-print(nombre_usuario * numero_entero)"""
+repeticion = nombre * entero
 
-#2 
+print(repeticion)"""
+
+#2
 """
-nombre_usuario = "Josias"
-apellido_usuario = "Caceres"
+nombre = 'Juan'
+apellido = 'Arias'
 
-nombre_completo = nombre_usuario + " " + apellido_usuario
+nombre_apellido = nombre + ' ' + apellido
 
-numero_entero = 10
+veces = 10
 
-print((nombre_completo + '\n') * numero_entero)"""
+print((nombre_apellido + '\n') * veces)
+"""
 
 #3
 """
-nombre = 'Josias'
+nombre = 'Samuel'
 apellido = 'Caceres'
 
 nombre_completo = nombre + ' ' + apellido
+nombre_completo_sin_espacio = nombre + apellido
 
-n = len(nombre_completo.replace(' ', ''))
+n = len(nombre_completo_sin_espacio)
 
-print(nombre_completo + ' tiene ' + str(n) + ' letras.')
+print(nombre_completo + ' tiene ' + str(n) + " letras.")
 """
 
 #4
 """
-producto = input('Ingrese el nombre del producto: ')
-precio_unitario = int(input('Ingrese el precio unitario del producto: '))
-cantidad = int(input('Ingrese la cantidad de unidades: '))
+#Asignar los valores a la variable
+producto = 'Manzana'
+cantidad = 10
+precio_unitario = 5000
 
+#Calcular precio total
 precio_total = precio_unitario * cantidad
 
-cantidad_str = str(cantidad)
+#Convertir valores a STR
 precio_unitario_str = str(precio_unitario)
+cantidad_str = str(cantidad)
 precio_total_str = str(precio_total)
 
-longitud_precio_unitario_str = len(precio_unitario_str)
-longitud_cantidad_str = len(cantidad_str)
-longitud_precio_total_str = len(precio_total_str)
+#sacar la longitud de los valores en STR
+long_precio_unitario_str = len(precio_unitario_str)
+long_cantidad_str = len(cantidad_str)
+long_precio_total_str = len(precio_total_str)
 
-precio_unitario_final = ('0' * (6 - longitud_precio_unitario_str)) + precio_unitario_str
-cantidad_final = ('0' * (3 - longitud_cantidad_str)) + cantidad_str
-precio_total_final = ('0' * (8 - longitud_precio_total_str)) + precio_total_str
+#agregamos los 0 faltantes a los valores
+ceros_precio_unitario = 6 - long_precio_unitario_str
+precio_unitario_final = ('0' * ceros_precio_unitario) + precio_unitario_str
 
+ceros_cantidad = 3 - long_cantidad_str
+cantidad_final = ('0' * ceros_cantidad) + cantidad_str
+
+ceros_precio_total = 8 - long_precio_total_str
+precio_total_final = ('0' * ceros_precio_total) + precio_total_str
+
+#Guardar en una variable el caracter guion
 guion = '_'
 
+#Hacemos el print final
 print(producto + guion + precio_unitario_final + guion + cantidad_final + guion + precio_total_final)
+
 """
 
 #5
 """
-frase = 'hola a todos'
-vocal = 'a' 
-frase += vocal.upper()
+frase = 'hola mundo'
+vocal = 'z'
 
-print(frase)"""
+vocal = vocal.upper()
+
+frase += vocal
+
+print(frase)
+"""
 
 #6
 """
-texto = 'Hola Mundo'
+texto = 'Hola mundo'
 n = 5
 
-print((texto + ' ') * n)"""
+print((texto + ' ') * n)
+"""
 
 #7
-"""nombre_libro = 'Harry Potter'
+"""
+libro = 'harry potter'
 
-cantidad_letras = len(nombre_libro.replace(' ', ''))
+libro_sin_espacios = libro.replace(' ' , '')
 
-print('El libro ' + nombre_libro + ' tiene ' + str(cantidad_letras) + 'caracteres.')"""
+cantidad_caracteres = len(libro_sin_espacios)
+
+print('El libro ' + libro.title() + ' tiene ' + str(cantidad_caracteres) + ' letras.')"""
